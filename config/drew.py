e9841dd8782b5b828832a5c8597369db3f42c9df
Drew Nguyen <drew.nguyen@me.com>
2025-11-23 09:21:14 -0800

    update

---
+++ config/drew.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def main():
+    print("this is a thing")
+    print("this is another line")
+    print("this is another thing"):
+
+
+
+if __name__ == "__main__":
+    print("Hello, Drew!")
+    print("this is another thing")
+    printkj
